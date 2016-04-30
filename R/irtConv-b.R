28cd6049056a68b61246962fc8d1780a252b5c08
ppanko <pavelppanko@gmail.com>
2016-04-29 19:20:04 -0500

    Vers 0.0.0.3-1

--- R/irtConv-b.R
+++ R/irtConv-b.R
@@ -2,6 +2,7 @@ namNami <- function(t, i) {
     if(t == "icc") title <- paste0("Item Characteristic Curves \n for the ", i, " Items")
     else if(t == "iif") title <- paste0("Item Information Functions \n for the ", i, " Items")
     else if(t == "tcc") title <- paste0("Test Characteristic Curve \n for the ", i, " Items")
+    else if(t == "tif") title <- paste0("Test Information Function \n for the ", i, " Items")
 }
 
 namNaml <- function(t, i) {
