5d0b458bd8ce15ce2d3af223d9bcf061ae80b02d
ppanko <pavelppanko@gmail.com>
2016-05-04 10:09:38 -0500

    0.0.1

---
+++ R/difConv.R
@@ -0,0 +1,39 @@
+crtFrmu <- function(x) {
+    dt <- list()
+    dt$a <- rep(x[ind+1], ind)
+    dt$b <- x[1:ind]
+    dt$c <- rep(0, ind)
+    return(dt)
+}
+
+crtFrmn <- function(x) {
+    dt <- list()
+    dt$a <- x[(ind+1):length(x)]
+    dt$b <- x[1:ind]
+    dt$c <- rep(0, ind)
+    return(dt)
+}
+
+calcD <- function(dat, tht = theta) {
+    iprb <- list()
+    for(i in 1:length(dat[[1]])) {
+        iprb[[i]] <- dat$c[i] + (1 - dat$c[i])*((exp(1.7*dat$a[i]*(tht-dat$b[i])))/(1+(exp(1.7*dat$a[i]*(tht-dat$b[i])))))
+    }
+    return(iprb)
+}
+
+dbind <- function(x) {
+        itms <- data.frame(prb = unlist(x),  grp = rep(names(hi2), each = length(theta)), tht = theta)
+}
+
+plotDif <- function(itms, ttl, x1 = itms$tht, y1 = itms$prb, grp, ylbs = ylb, lgd = "Group") {
+
+    ggplot2::ggplot(itms, aes(x = x1, y = y1)) +
+        geom_line(aes(color = grp), size = 1) +
+        ggtitle(paste(ttl, '\n')) +
+        xlab(expression(atop(,Ability(theta)))) +
+        scale_x_continuous(breaks = seq(min(x1), max(x1), 1)) +
+        ylab(ylbs) +
+        theme(axis.title = element_text(size = 14, face = "italic"), title = element_text(size = 15, face = "bold")) +
+        guides(color=guide_legend(title=lgd))
+}
---
+++ R/difPlot-b.R
@@ -0,0 +1,48 @@
+difPlot <- function(dat,
+                    theta = seq(-3,3, 0.01),
+                    title = NULL,
+                    ddir = getwd(),
+                    save = FALSE,
+                    model,
+                    fln = paste0(model, "_", "dif_", colnames(dat), ".jpg"),
+                    dpi = 800,
+                    height = 6.5,
+                    width = 10,
+                    itmNam = colnames(dat))
+
+{
+
+    if(length(title) < 1) title2 <- sapply(itmNam, function(x) paste0("Item Characteristic Functions \n for the ", x, " Item Between Groups"))
+    else title2 <- title
+
+    ylb <- expression(atop(P(theta),))
+    ind <- ncol(dat)
+
+    if(model == "1PL") {
+
+        out <- by(dat, grp, function(x) summary(ltm::rasch(x))$coefficients[,1])
+        cf <- lapply(out, crtFrmu)
+
+    } else if(model == "2PL") {
+
+        out <- by(dat, grp, function(x) summary(ltm::ltm(x ~ z1))$coefficients[,1])
+        cf <- lapply(out, crtFrmn)
+
+    } else stop("Please enter a valid plot type, comrade")
+
+    prb <- lapply(cf, calcD)
+
+    for(j in 1:length(prb)) {
+        for(i in 1:length(prb)) {
+            prb[[j]][[i]] <- prb[[i]][[j]]
+        }
+    }
+
+    itms <- lapply(prb, dbind)
+
+    itmplot <- mapply(plotDif, itms, title2, SIMPLIFY = FALSE)
+
+    if(save == TRUE) mapply(gSave, x = plPlot , flnm = fln, dDir = ddir, res = dpi, hgt = height, wdt = width)
+
+    print(lapply(plPlot, prints))
+}
